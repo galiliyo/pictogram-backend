fe555131de03357b157d2599c1506ab87d339c89
galiliyo <yon.galili@gmail.com>
2019-09-11 13:03:40 +0300

    heroku

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,7 +1,6 @@
 const logger = require('../services/LoggerService')
 
 async function requireAuth(req, res, next) {
-  // console.log('req.session.user at auth',req.session);
   
   if (!req.session || !req.session.user) {
     res.status(401).end('Unauthorized, Auth required');
